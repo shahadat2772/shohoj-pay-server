4a3fd235f3a71341badde731ea49ce5a24e5f644
shahadat2772 <shahadat2772@gmailcom>
2022-07-25 23:27:18 +0600

    setup the backend and connect mongodb

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const port = process.env.PORT || 5000;
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+
+// MiddleWere
+app.use(express.json());
+app.use(cors());
+
+// Mongodb
+const { MongoClient, ServerApiVersion } = require("mongodb");
+// URI
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0lpuf.mongodb.net/?retryWrites=true&w=majority`;
+// Client info
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  console.log("HELLO");
+  try {
+    await client.connect();
+  } finally {
+    // await client.close();
+  }
+}
+
+run().catch(console.dir);
+
+// Initial API
+app.get("/", (req, res) => {
+  res.send("Hello there!");
+});
+
+// Listening port
+app.listen(port, () => {
+  console.log("Responding to", port);
+});
