7511fdabf4cfccf9b6cafaf63227a1ecad1a90ff
shahadat2772 <shahadat2772@gmailcom>
2022-08-04 16:20:40 +0600

    added validetion for send money feature

--- controllers/services.controller.js
+++ controllers/services.controller.js
@@ -1,20 +1,31 @@
 const balanceCollection = require("../models/balances.model");
 const savingCollection = require("../models/savings.model");
 const transactionCollection = require("../models/transactions.model");
+const userCollection = require("../models/users.model");
 const date = new Date().toLocaleDateString();
 
 const updateBalance = async (email, amount) => {
   const balanceInfo = await balanceCollection.findOne({ email });
   const lastBalance = parseInt(balanceInfo?.balance);
   const newBalance = (lastBalance + amount).toString();
+  if (parseInt(newBalance) < 0) {
+    return {
+      message: "insufficient",
+    };
+  }
   const doc = {
     $set: {
       balance: newBalance,
     },
   };
   const result = await balanceCollection.updateOne({ email }, doc);
-  return result;
+  if (result.modifiedCount > 0) {
+    return {
+      message: "success",
+    };
+  }
 };
+
 const updateSaving = async (email, amount) => {
   const savingInfo = await savingCollection.findOne({ email });
   const lastSaving = parseInt(savingInfo?.saving);
@@ -32,12 +43,17 @@ const addStatement = async (statement) => {
   return result;
 };
 
+const isExists = async (email) => {
+  const user = await userCollection.findOne({ email });
+  return user;
+};
+
 // ADD MONEY
 exports.addMoney = async (req, res) => {
   const { addMoneyInfo } = req.body;
   const email = addMoneyInfo?.email;
   const amount = parseInt(addMoneyInfo?.amount);
-  const result = updateBalance(email, amount);
+  const result = await updateBalance(email, amount);
   res.send(result);
 };
 
@@ -46,10 +62,26 @@ exports.sendMoney = async (req, res) => {
   const { sendMoneyInfo } = req?.body;
   const sendersEmail = sendMoneyInfo?.from;
   const receiversEmail = sendMoneyInfo?.to;
+  const receiversInfo = await isExists(receiversEmail);
+  if (!receiversInfo) {
+    res.send({
+      error: "Receiver not found.",
+    });
+    return;
+  }
   const amount = parseInt(sendMoneyInfo?.amount);
-  const updateSendersBalanceResult = updateBalance(sendersEmail, -amount);
-  const sendersStatementResult = addStatement(sendMoneyInfo);
-  const updateReceiversBalanceResult = updateBalance(receiversEmail, amount);
+  const updateSendersBalanceResult = await updateBalance(sendersEmail, -amount);
+  if (updateSendersBalanceResult.message === "insufficient") {
+    res.send({
+      error: "Insufficient balance.",
+    });
+    return;
+  }
+  const sendersStatementResult = await addStatement(sendMoneyInfo);
+  const updateReceiversBalanceResult = await updateBalance(
+    receiversEmail,
+    amount
+  );
   const receiversStatement = {
     type: "receiveMoney",
     email: receiversEmail,
@@ -58,13 +90,14 @@ exports.sendMoney = async (req, res) => {
     amount: sendMoneyInfo?.amount,
     date: date,
   };
-  const receiversStatementResult = addStatement(receiversStatement);
-  res.send({
-    updateSendersBalanceResult,
-    sendersStatementResult,
-    updateReceiversBalanceResult,
-    receiversStatementResult,
-  });
+  const receiversStatementResult = await addStatement(receiversStatement);
+  if (
+    sendersStatementResult?.insertedId &&
+    updateReceiversBalanceResult?.message === "success" &&
+    receiversStatementResult?.insertedId
+  ) {
+    res.send({ success: `$${amount} sended success fully.` });
+  }
 };
 
 // Save Money
