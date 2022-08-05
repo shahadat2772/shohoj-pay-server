fa461da24e9608e7fd14b2593b279098cac265fa
mahfuzswaron <mahfuzswaron@gmail.com>
2022-08-05 10:01:42 +0600

    user settings controller done

--- routes/users.route.js
+++ routes/users.route.js
@@ -2,10 +2,12 @@ const express = require("express");
 const {
   createAccount,
   getUserInfo,
+  updateUserInfo,
 } = require("../controllers/users.controller");
 const router = express.Router();
 
 router.post("/createAccount", createAccount);
 router.get("/getUserInfo", getUserInfo);
+router.put("/updateUserInfo", updateUserInfo)
 
 module.exports = router;
