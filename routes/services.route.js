ba5bbf8fa1f7bdc373bba70b6e55e0110ba14a8b
Minhazul Alam <78158511+akib-chy@users.noreply.github.com>
2022-08-23 12:03:01 +0600

    personal to merchant pay implement done

--- routes/services.route.js
+++ routes/services.route.js
@@ -7,7 +7,12 @@ const { eCheckInfo } = require("../controllers/services/eCheck.controller");
 const {
   getServices,
 } = require("../controllers/services/getServices.controller");
-const { mechantToPersonal } = require("../controllers/services/merchantToPersonal.controller");
+const {
+  mechantToPersonal,
+} = require("../controllers/services/merchantToPersonal.controller");
+const {
+  personalToMerchant,
+} = require("../controllers/services/personalToMerchant.controller");
 
 const {
   requestMoney,
@@ -42,6 +47,13 @@ router.get("/getServices", getServices);
 // Merchant Services Routes
 
 // Merchant to Personal
-router.post("/merchant-to-personal", verifyJWT, verifyMerchant, mechantToPersonal)
+router.post(
+  "/merchant-to-personal",
+  verifyJWT,
+  verifyMerchant,
+  mechantToPersonal
+);
+// Personal to Merchant
+router.post("/personal-to-merchant", personalToMerchant);
 
 module.exports = router;
