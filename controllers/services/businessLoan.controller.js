2bf1c532bb240e88535822bc87c045cf4cebe311
mahfuz swaron <mahfuzswaron@gmail.com>
2022-08-25 19:37:39 -0400

    business loan validation done

--- controllers/services/businessLoan.controller.js
+++ controllers/services/businessLoan.controller.js
@@ -1,4 +1,17 @@
+const loanCollection = require("../../models/loan.model");
 exports.requestBusinessLoan = async (req, res) => {
-    console.log(req.body)
-    res.send({ success: "cinta koiren na, peye jaben" })
+    const loanInfo = req.body;
+    const requester = loanInfo.requester;
+    const existingRequests = await loanCollection.find({ requester: requester }).toArray()
+    if (existingRequests.length === 0) {
+        const insertResult = await loanCollection.insertOne(loanInfo);
+        if (insertResult.insertedId) {
+            res.send({ success: "Your request is will be reviewed soon." })
+        }
+    }
+    else {
+        res.send({ error: "Sorry, you already have a loan request." })
+    }
+
+
 }
