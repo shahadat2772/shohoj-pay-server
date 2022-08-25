38bf0b5fd9261c960bc3c6e90a9fccfd86938cb0
mahfuz swaron <mahfuzswaron@gmail.com>
2022-08-24 21:49:55 -0400

    business loan initialized

--- controllers/services/businessLoan.controller.js
+++ controllers/services/businessLoan.controller.js
@@ -1,4 +1,4 @@
 exports.requestBusinessLoan = async (req, res) => {
     console.log(req.body)
-    res.send({ message: "cinta koiren na, peye jaben" })
+    res.send({ success: "cinta koiren na, peye jaben" })
 }
