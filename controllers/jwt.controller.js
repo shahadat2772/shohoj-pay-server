fad9b7fad649688a5417efbbed58e3dd153e43f6
shahadat2772 <shahadat2772@gmailcom>
2022-08-26 08:33:55 +0600

    updated the jwt provider async fucntion

--- controllers/jwt.controller.js
+++ controllers/jwt.controller.js
@@ -8,23 +8,13 @@ exports.verifyJWT = async (req, res, next) => {
     return res.status(401).send({ message: "UnAuthorize Access" });
   }
   const token = authHeader.split(" ")[1];
-  jwt.verify(
-    token,
-    process.env.JWT_SECRET_TOKEN,
-    async function (err, decoded) {
-      if (err) {
-        return res.status(403).send({ message: "Forbidden Access" });
-      }
-      const userInfo = await getUserInfo(decoded);
-      if (!userInfo?.status === "active") {
-        return res.status(403).send({
-          error: "Your account was deactivated, contact us to know more.",
-        });
-      }
-      req.decoded = decoded;
-      next();
+  jwt.verify(token, process.env.JWT_SECRET_TOKEN, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden Access" });
     }
-  );
+    req.decoded = decoded;
+    next();
+  });
 };
 
 exports.getJwtToken = async (req, res) => {
