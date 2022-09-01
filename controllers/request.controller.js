74fc54f8238e9bea6db5108a442097b7f200128c
shahadat2772 <shahadat2772@gmailcom>
2022-09-01 23:34:42 +0600

    fixed the back3dnn

--- controllers/request.controller.js
+++ controllers/request.controller.js
@@ -1,9 +1,14 @@
 const requestCollection = require("../models/moneyRequests.model");
 
 exports.getRequests = async (req, res) => {
-  //   const email = req.headers?.email;
-  //   const type = req.headers?.type;
-  //   const requests = requestCollection.find({ to: email });
-  const request = await requestCollection.find().toArray();
-  res.send(request);
+  const email = req.headers?.email;
+  const type = req.headers?.type;
+  let filter;
+  if (type === "incoming") {
+    filter = { to: email };
+  } else {
+    filter = { from: email };
+  }
+  const requests = requestCollection.find(filter);
+  res.send(requests);
 };
