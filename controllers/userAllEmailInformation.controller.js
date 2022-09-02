94118dd1669f37ee314e801007439a3adb21f618
mahfuz swaron <mahfuzswaron@gmail.com>
2022-09-02 13:02:26 -0400

    get transaction amount by type done

--- controllers/userAllEmailInformation.controller.js
+++ controllers/userAllEmailInformation.controller.js
@@ -21,3 +21,22 @@ exports.getAllTransaction = async (req, res) => {
     .toArray();
   res.send(userTransactionInfo);
 };
+
+exports.getTransactionAmountByType = async (req, res) => {
+  const email = req.params.email;
+  const addedMoney = await transactionCollection.find({ email: email, type: "Add Money" }).toArray();
+  const gotPaid = await transactionCollection.find({ email: email, type: "Receive Money", }).toArray();
+  const mToM = await transactionCollection.find({ email: email, type: "M to M", }).toArray();
+  const mToP = await transactionCollection.find({ email: email, type: "M to P", }).toArray();
+  const getSum = (total, amount) => parseFloat(total) + parseFloat(amount);
+  const totalAddedMoney = await addedMoney.map(x => x.amount).reduce(getSum, 0);
+  const totalgotPaid = await gotPaid.map(x => x.amount).reduce(getSum, 0);
+  const totalmToM = await mToM.map(x => x.amount).reduce(getSum, 0);
+  const totalmToP = await mToP.map(x => x.amount).reduce(getSum, 0)
+
+  res.send([
+    { name: "Added", value: totalAddedMoney, color: "#0088FE" },
+    { name: "Got Paid", value: totalgotPaid, color: "#00C49F" },
+    { name: "Sent", value: totalmToM + totalmToP, color: "#FFBB28" },
+  ])
+}
